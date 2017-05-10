ae42ad3cc9e69944b81948ee3d0c986352174613
Matt Schuchard <grapheneowns@gmail.com>
2017-05-10 14:50:45 -0400

    updating ci

--- spec/linter-icinga-validate-spec.js
+++ spec/linter-icinga-validate-spec.js
@@ -52,6 +52,43 @@ describe('The Icinga Validate provider for Linter', () => {
     });
   });
 
+  describe('checks a file with a warning', () => {
+    let editor = null;
+    const badFile = path.join(__dirname, 'fixtures', 'warning.conf');
+    beforeEach(() => {
+      waitsForPromise(() =>
+        atom.workspace.open(badFile).then(openEditor => {
+          editor = openEditor;
+        })
+      );
+    });
+
+    it('finds the first message', () => {
+      waitsForPromise(() =>
+        lint(editor).then(messages => {
+          expect(messages.length).toEqual(1);
+        })
+      );
+    });
+
+    it('verifies the first message', () => {
+      waitsForPromise(() => {
+        return lint(editor).then(messages => {
+          expect(messages[0].type).toBeDefined();
+          expect(messages[0].type).toEqual('Warning');
+          expect(messages[0].text).toBeDefined();
+          expect(messages[0].text).toEqual("Apply rule 'satellite-host' for type 'Dependency' does not match anywhere!']'");
+          expect(messages[0].filePath).toBeDefined();
+          expect(messages[0].filePath).toMatch(/.+warning\.conf$/);
+          expect(messages[0].range).toBeDefined();
+          expect(messages[0].range.length).toBeDefined();
+          expect(messages[0].range.length).toEqual(2);
+          expect(messages[0].range).toEqual([[3, 3], [3, 4]]);
+        });
+      });
+    });
+  });
+
   it('finds nothing wrong with a valid file', () => {
     waitsForPromise(() => {
       const goodFile = path.join(__dirname, 'fixtures', 'clean.conf');
